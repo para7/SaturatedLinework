b5e3f128780c4efade14b22498b283f8ef3c2221
para7 <minstrel.suppin@gmail.com>
2019-09-28 17:01:50 +0900

    progress

--- Main.cpp
+++ Main.cpp
@@ -23,8 +23,6 @@ Ellipse::position_type GetPointOnLine(const Ellipse& ellipse, double angle)
 template <class T>
 static Vec2 GetPointOnLine(const Rectangle<T>& rect, double angle)
 {
-    
-    
     //line begin, line end
     Vec2 lbegin, lend;
     constexpr auto pi4 = Math::Pi / 4;
@@ -71,15 +69,28 @@ static Vec2 GetPointOnLine(const Rectangle<T>& rect, double angle)
 }
 
 
-
+// 集中線クラス
 template <class InnerShape, class OuterShape>
-struct ConcentratedEffect
+class ConcentratedEffect
 {
+public:
+    
+    //内側の図形
     InnerShape innershape;
+    //外側の図形
     OuterShape outershape;
     
-    int linenum;
-    double thickness = 10;
+    //線の数
+    int linenum = 60;
+    
+    //線の太さ 中心からの角度
+    double thicknessangle = 0.01;
+    
+    //太さのランダム幅
+    double thickrandomness = 0.003;
+    
+    //出現位置のランダム幅
+    double posrandomness = 0;
     
 private:
     
@@ -91,10 +102,15 @@ public:
     : innershape(_innershape)
     , outershape(_outershape)
     {
-        linenum = 60;
-        
         Generate();
     }
+//    
+//    ConcentratedEffect(const InnerShape& _innershape, const OuterShape& _outershape)
+//    : innershape(_innershape)
+//    , outershape(_outershape)
+//    {
+//        (Vec2(Scene::Size()) / 2, Vec2(Scene::Size()) * sqrt(2.0));
+//    }
     
     
     //outershapeを指定しない場合は画面をカバーするように自動設定する
@@ -113,16 +129,13 @@ public:
         
         for(int i : step(linenum))
         {
-            double angle = Random(2 * Math::Pi);
+            const double angle = Random(2 * Math::Pi);
             
             auto inner = GetPointOnLine(innershape, angle);
-            auto outer = GetPointOnLine(outershape, angle);
             
-            auto shifted_angle = angle - Math::Pi/2;
-            auto outerleft = outer.movedBy(cos(shifted_angle) * thickness, sin(shifted_angle) * thickness);
+            auto outerleft = GetPointOnLine(outershape, angle + thicknessangle);
             
-            shifted_angle = angle + Math::Pi/2;
-            auto outerright = outer.movedBy(cos(shifted_angle) * thickness, sin(shifted_angle) * thickness);
+            auto outerright = GetPointOnLine(outershape, angle - thicknessangle);
             
             m_triangles.emplace_back(inner, outerleft, outerright);
         }
@@ -135,80 +148,38 @@ public:
             triangle.draw(color);
         }
     }
-    
-    static void test()
-    {
-        
-    }
 };
 
 void Main()
 {
     Window::Resize(1280, 720);
     
-    Ellipse el(400, 200, 200, 100);
-    Ellipse windowel(Vec2(Scene::Size()) / 2, Vec2(Scene::Size()) * sqrt(2.0));
+    Ellipse el(400, 350, 100, 50);
+    Ellipse el2(400, 350, 100*3, 50*3);
+    //Ellipse windowel(Vec2(Scene::Size()) / 2, Vec2(Scene::Size()) * sqrt(2.0));
     
-    ConcentratedEffect effect(el, windowel);
+    ConcentratedEffect effect(el, el2);
     
     Scene::SetBackground(HSV(0, 0, 0.9));
     
-    Font font(130, Typeface::Default, FontStyle::BoldItalic);
+    Font font(50, Typeface::Default, FontStyle::BoldItalic);
     
     double num = 0.3;
     
     while(System::Update())
     {
-        //
-        //        font(U"集中線").drawAt(el.center, Palette::Black);
-        //
-        //        effect.draw(Palette::Black);
-        //
-        //        SimpleGUI::Slider(U"{:.0f}"_fmt(num), num, 0.0, 300.0, Vec2(100, 80), 60, 150);
-        //
-        //        effect.linenum = static_cast<int>(num);
-        //
-        //        if(SimpleGUI::Button(U"Generate", Vec2(100, 40)))
-        //        {
-        //            effect.Generate();
-        //        }
         
-        static double p;
-        p += Math::Pi * 0.0075;
+        font(U"集中線").drawAt(el.center, Palette::Black);
         
-        Vec2 pos;
+        effect.draw(Palette::Black);
         
+        SimpleGUI::Slider(U"{:.0f}"_fmt(num), num, 0.0, 300.0, Vec2(100, 80), 60, 150);
+        
+        effect.linenum = static_cast<int>(num);
+        
+        if(SimpleGUI::Button(U"Generate", Vec2(100, 40)))
         {
-            RectF r(700,100,400,300);
-            r.draw();
-            
-            pos = GetPointOnLine(r,  p);
-            
-            if(KeySpace.pressed()){
-                Line(pos, r.center()).draw(Palette::Green);
-            }
-            Circle(pos, 10).draw(Palette::Red);
+            effect.Generate();
         }
-        {
-            RectF r(300,400,100,300);
-            r.draw();
-            
-            pos = GetPointOnLine(r,  p);
-            
-            if(KeySpace.pressed()){
-                Line(pos, r.center()).draw(Palette::Green);
-            }
-            Circle(pos, 10).draw(Palette::Red);
-        }
-        
-        pos = GetPointOnLine(el,  p);
-        
-        el.draw();
-        if(KeySpace.pressed()){
-            Line(pos, el.center).draw(Palette::Green);
-        }
-        
-        Circle(pos, 10).draw(Palette::Red);
     }
-    
 }
