83e558605680ed62a05de4927310a90096089984
para7 <minstrel.suppin@gmail.com>
2019-09-30 15:42:26 +0900

    fix

--- Main.cpp
+++ Main.cpp
@@ -25,37 +25,54 @@ public:
     OuterShape outershape;
     
     //線の数
-    int linenum = 60;
+    int linenum = 70;
     
     //線の太さ 中心からの角度
-    double thickness = 10;
+    double thickness = 7;
     
     //太さのランダム幅
-    double thickrandomness = 7;
+    double thickrandomness = 16;
     
     //出現位置のランダム幅
-    double posrandomness = 50;
+    double posrandomness = 40;
     
 private:
     
     Array<Triangle> m_triangles;
     
-public:
+public: //constructor
+    
+    ConcentratedEffect(const InnerShape& _innershape, const OuterShape& _outershape)
+    : innershape(_innershape)
+    , outershape(_outershape)
+    {
+        Generate();
+    }
+    
+    //outershapeを指定しない場合は画面をカバーするように自動設定する
+    ConcentratedEffect(const InnerShape& _innershape)
+    : innershape(_innershape)
+    , outershape(Rect(0, 0, Scene::Size()).stretched(20, 20))
+    {
+        Generate();
+    }
+    
+public: //func
     
     //集中線を生成する
     void Generate()
     {
+        //内部の図形の方が外部の図形より大きい場合はエラー
+        //        if(!outershape.contains(innershape))
+        //        {
+        //            Print(U"error");
+        //            return;
+        //            //エラー処理
+        //        }
+        
         m_triangles.clear();
         m_triangles.reserve(linenum);
 
-        //内部の図形の方が外部の図形より大きい場合はエラー
-//        if(!outershape.contains(innershape))
-//        {
-//            Print(U"error");
-//            return;
-//            //エラー処理
-//        }
-        
         //交差座標取得用
         OffsetCircular3 cir;
         //半径を長く取る
@@ -72,26 +89,30 @@ public:
             
             //内側の座標
             //まずはランダム性を持たせる
-            //const auto is = innershape.stretched(Random(posrandomness));
-            //const auto inner = GetPointOnLine(is, angle);
-            
+            const auto is = innershape.stretched(Random(posrandomness));
             //InnerShapeの中心から線を始めているので、intersectsAtの存在判定は不要
-            const Vec2 inner = innershape.intersectsAt(line).value().front();
+            const Vec2 inner = is.intersectsAt(line).value().front();
             
-            //外側の座標を計算する
-            //こっちは存在判定が必要だが、outershapeがinnershapeを内包しているかどうかで判定すべきである
+            //外側の中心(基準)となる座標を計算する
+            //こっちは存在しない可能性があるが、 outershape が innershape を内包しているかどうかで判定すべきである
             //ただしEllipse.contains()はないのでどうするか
             const auto outer = outershape.intersectsAt(line).value().front();
+            
+            //こっちも極座標にしようとしたら座標計算ミスしまくったのでとりあえずこのままで
             
             //90度回す
             const auto rotated = angle + (Math::Pi / 2);
             
-            //太さの分ずらすベクトルを作成
+            //ずらす量は太さの半分
             const double r = ( (thickness + Random(thickrandomness)) / 2);
+            
+            //単位ベクトルを生成
             const Vec2 v(cos(rotated), sin(rotated));
+            
+            //実際のずらす量を計算
             const Vec2 outeroffset = v * r;
             
-            //ずらした座標を作る
+            //左右にずらした座標を作る
             const Vec2 outerleft = outer + outeroffset;
             const Vec2 outerright = outer - outeroffset;
             
@@ -108,19 +129,9 @@ public:
         }
     }
     
-    ConcentratedEffect(const InnerShape& _innershape, const OuterShape& _outershape)
-    : innershape(_innershape)
-    , outershape(_outershape)
+    void GenerateDraw(const Color& color) const
     {
-        Generate();
-    }
-    
-    //outershapeを指定しない場合は画面をカバーするように自動設定する
-    ConcentratedEffect(const InnerShape& _innershape)
-    : innershape(_innershape)
-    , outershape(Rect(0, 0, Scene::Size()).stretched(20, 20))
-    {
-        Generate();
+        
     }
 };
 
@@ -128,16 +139,19 @@ void Main()
 {
     Window::Resize(1280, 720);
     
-    Ellipse el(200, 500, 300, 100);
-    Ellipse el2 = el.stretched(800);
+    Ellipse el(400, 400, 300, 100);
+    //Ellipse el2 = el.stretched(800);
     
-    ConcentratedEffect effect(el, el2);
+    ConcentratedEffect effect(el);
     
     Scene::SetBackground(HSV(0, 0, 0.93));
     
     Font font(90, Typeface::Default, FontStyle::BoldItalic);
     
-    double num = 0.3;
+    double num = effect.linenum;
+    double thick = effect.thickness;
+    double posrandom = effect.posrandomness;
+    double thickrandom = effect.thickrandomness;
     
     while(System::Update())
     {
@@ -145,11 +159,23 @@ void Main()
         
         effect.draw(Palette::Black);
         
-        SimpleGUI::Slider(U"{:.0f}"_fmt(num), num, 0.0, 300.0, Vec2(100, 80), 60, 150);
+        const int label = 130;
+        const int slider = 250;
+        SimpleGUI::Slider(U"num{:.0f}"_fmt(num), num, 0.0, 300.0, Vec2(100, 80), label, slider);
+        SimpleGUI::Slider(U"thick{:.0f}"_fmt(thick), thick, 0.0, 80.0, Vec2(100, 80 + 40 * 1), label, slider);
+        SimpleGUI::Slider(U"posRand{:.0f}"_fmt(posrandom), posrandom, 0.0, 400.0, Vec2(100, 80 + 40 * 2), label, slider);
+        SimpleGUI::Slider(U"thickRand{:.0f}"_fmt(thickrandom), thickrandom, 0.0, 50.0, Vec2(100, 80 + 40 * 3), label, slider);
         
         effect.linenum = static_cast<int>(num);
+        effect.thickness = static_cast<int>(thick);
+        effect.thickrandomness = static_cast<int>(thickrandom);
+        effect.posrandomness = static_cast<int>(posrandom);
         
         if(SimpleGUI::Button(U"Generate", Vec2(100, 40)))
+        {
+            effect.Generate();
+        }
+        if(SimpleGUI::Button(U"RandomSet", Vec2(260, 40)))
         {
             effect.Generate();
         }
