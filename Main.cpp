e81d6003f73cfcfd2a06ff5ca7300607e18e49d6
para7 <minstrel.suppin@gmail.com>
2019-09-30 14:26:21 +0900

    bug generator ver

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,9 @@
 
 #include <Siv3D.hpp>
+#include <HamFramework.hpp>
 
 //角度の正規化 0~2π
-constexpr double AngleNormalize(double angle)
+double AngleNormalize(double angle)
 {
     while(angle < 0)
     {
@@ -12,6 +13,7 @@ constexpr double AngleNormalize(double angle)
     return fmod(angle, Math::Pi * 2);
 }
 
+//Ellipseの線上の点を求める
 Ellipse::position_type GetPointOnLine(const Ellipse& ellipse, double angle)
 {
     auto x = cos(angle);
@@ -20,57 +22,50 @@ Ellipse::position_type GetPointOnLine(const Ellipse& ellipse, double angle)
     return Ellipse::position_type(x * ellipse.a, y * ellipse.b).moveBy(ellipse.center);
 }
 
+//Rectの線上の点を求める
 template <class T>
 static Vec2 GetPointOnLine(const Rectangle<T>& rect, double angle)
 {
-    //line begin, line end
-    Vec2 lbegin, lend;
     constexpr auto pi4 = Math::Pi / 4;
     
     angle = AngleNormalize(angle);
     
+    Line line;
+    
     //辺を選択する
-    //topとbottomが入れ替わっているのは時計回りに配置し直したいため
-    //ついでに角度を0~1/2πに正規化
+    //ついでに角度を0~1/2πに正規化する
     if(angle < pi4 || 7 * pi4 < angle)
     {
-        const auto line = rect.right();
-        lbegin = line.end;
-        lend = line.begin;
+        line = rect.right();
         angle += pi4;
         angle = AngleNormalize(angle);
     }
     else if(angle < 3 * pi4)
     {
-        const auto line = rect.top();
-        lbegin = line.end;
-        lend = line.begin;
+        line = rect.top();
         angle -= pi4;
     }
     else if(angle < 5 * pi4)
     {
-        const auto line = rect.left();
-        lbegin = line.end;
-        lend = line.begin;
+        line = rect.left();
         angle -= pi4 * 3;
     }
     else
     {
-        const auto line = rect.bottom();
-        lbegin = line.end;
-        lend = line.begin;
+        line = rect.bottom();
         angle -= pi4 * 5;
     }
+    
     //角度をさらに0~1に正規化
     const auto v = (angle * 2) / Math::Pi;
     
     //線形補間で座標を出す
-    return lbegin.lerp(lend, v);
+    return line.end.lerp(line.begin, v);
 }
 
 
 // 集中線クラス
-template <class InnerShape, class OuterShape>
+template <class InnerShape, class OuterShape = Rect>
 class ConcentratedEffect
 {
 public:
@@ -84,13 +79,13 @@ public:
     int linenum = 60;
     
     //線の太さ 中心からの角度
-    double thickness = 8;
+    double thickness = 6;
     
     //太さのランダム幅
-    double thickrandomness = 0.1;
+    double thickrandomness = 0;
     
     //出現位置のランダム幅
-    double posrandomness = 0;
+//    double posrandomness = 100;
     
 private:
     
@@ -98,61 +93,47 @@ private:
     
 public:
     
-    ConcentratedEffect(const InnerShape& _innershape, const OuterShape& _outershape)
-    : innershape(_innershape)
-    , outershape(_outershape)
-    {
-        Generate();
-    }
-    //
-    //    ConcentratedEffect(const InnerShape& _innershape, const OuterShape& _outershape)
-    //    : innershape(_innershape)
-    //    , outershape(_outershape)
-    //    {
-    //        (Vec2(Scene::Size()) / 2, Vec2(Scene::Size()) * sqrt(2.0));
-    //    }
-    
-    
-    //outershapeを指定しない場合は画面をカバーするように自動設定する
-    //    ConcentratedEffect(const InnerShape& _innershape)
-    //        : innershape(_innershape)
-    //        , outershape(Rect(0, 0, Scene::Size()))
-    //    {
-    //        Generate();
-    //    }
-    
     //集中線を生成する
     void Generate()
     {
         m_triangles.clear();
         m_triangles.reserve(linenum);
+     
+        if(!outershape.contains(innershape))
+        {
+            Print(U"error");
+            return;
+            //エラー処理
+        }
         
         for(int i : step(linenum))
         {
+            //角度を作成
             const double angle = Random(2 * Math::Pi);
+            
+            //内側の座標
+            //まずはランダム性を持たせる
+            //const auto is = innershape.stretched(Random(posrandomness));
+            //const auto inner = GetPointOnLine(is, angle);
             
             const auto inner = GetPointOnLine(innershape, angle);
             
+            //外側の座標を計算する
             const auto outer = GetPointOnLine(outershape, angle);
             
             //90度回す
             const auto rotated = angle + (Math::Pi / 2);
             
-            if(KeySpace.down())
-            {
-                Print(angle * 180.0 / Math::Pi);
-                Print(rotated * 180.0 / Math::Pi);
-            }
+            //太さの分ずらすベクトルを作成
+            const auto r = ( (thickness + Random(thickrandomness)) / 2);
+            const Vec2 v(cos(rotated), -sin(rotated));
+            const auto outeroffset = v * r;
             
-            Print(rotated * 180.0 / Math::Pi);
-            
-            //太さの分ずらすベクトル
-            auto outeroffset = Vec2(cos(rotated), -sin(rotated)) * (thickness / 2);
-            
+            //ずらした座標を作る
             const auto outerleft = outer + outeroffset;
-            
             const auto outerright = outer - outeroffset;
             
+            //Triangleの座標として登録
             m_triangles.emplace_back(inner, outerleft, outerright);
         }
     }
@@ -164,21 +145,35 @@ public:
             triangle.draw(color);
         }
     }
+    
+    ConcentratedEffect(const InnerShape& _innershape, const OuterShape& _outershape)
+    : innershape(_innershape)
+    , outershape(_outershape)
+    {
+        Generate();
+    }
+    
+    //outershapeを指定しない場合は画面をカバーするように自動設定する
+    ConcentratedEffect(const InnerShape& _innershape)
+    : innershape(_innershape)
+    , outershape(Rect(0, 0, Scene::Size()).stretched(20, 20))
+    {
+        Generate();
+    }
 };
 
 void Main()
 {
     Window::Resize(1280, 720);
     
-    Ellipse el(400, 350, 100, 50);
-    Ellipse el2(400, 350, 100*3, 50*3);
-    //Ellipse windowel(Vec2(Scene::Size()) / 2, Vec2(Scene::Size()) * sqrt(2.0));
+    Ellipse el(200, 500, 200, 100);
+//    Ellipse el2(400, 350, 100*3, 50*3);
     
-    ConcentratedEffect effect(el, el2);
+    ConcentratedEffect effect(el);
     
     Scene::SetBackground(HSV(0, 0, 0.9));
     
-    Font font(50, Typeface::Default, FontStyle::BoldItalic);
+    Font font(90, Typeface::Default, FontStyle::BoldItalic);
     
     double num = 0.3;
     
